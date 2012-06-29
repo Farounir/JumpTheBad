0ab7fe57c8eb9b93619a7a63c1d14439855701b0
layzcurler <lazycurler@gmail.com>
2012-06-29 02:13:58 -0500

    I got rid of the stupid header file and its c counterpart cause opps is for fools

--- JTBGame.c
+++
@@ -1,2 +0,0 @@
-//#include "JTBGame.h"
-
--- jumpTheBad.ino
+++ jumpTheBad.ino
@@ -14,22 +14,40 @@
 
 // include the library code:
 #include <LiquidCrystal.h>
-#include "JTBGame.h"
 
+//CONSTANTS
 const int DELAY_TIME = 200;
-int jumPin = 6;  //pin to read for sweet jumping action!
-int buttonState = 0;  //variable for reading the button
-int lastState = 0;  //keeps track of the last state of the button
+int JUMPIN = 6;  //pin to read for sweet jumping action!
+const int WIDTH = 16;  //number of lcd characters long
+const int HEIGHT = 2;  //number of lcd characters high
 
-//custom graphics
-uint8_t manArray[] = {0xe,0xe,0x4,0xe,0x15,0xe,0x1b,0x00};
-uint8_t man = 0;
+//Globals...cause if pacman did it, we can too!
+int buttonState;  //variable for reading the button
+int lastButton;  //keeps track of the last state of the button
+bool alive; //keeps track of whether man is alive or not i.e. if game goes on
+int manPos; //keeps track if man is standing, jumping up, has jumped, or is landing using an int 0-4
 
-uint8_t obsArray0[] = {0x0,0x0,0x4,0xa,0x15,0xa,0x15, 0x00};
-uint8_t obs0 = 1;
+uint8_t lcdChar[WIDTH][HEIGHT]; //a 2d array that holds cust characters and is used to keep track of game state
 
-uint8_t obsArray1[] = {0x2,0xd,0x3,0xd,0x3,0xd,0x2, 0x00};
-uint8_t obs1 = 2;
+
+//custom graphics!!!!1!!!11!
+const int numChar = 8; //the number of characters needed to create a custom lcd character
+typedef uint8_t custChar[numChar];
+
+custChar MAN_A = {0xe,0xe,0x4,0xe,0x15,0xe,0x1b,0x00}; //standing man
+const uint8_t MAN = 0;
+
+custChar MAN_JUMP_A = {0xe,0xe,0x5,0xe,0x14,0xe,0x12,0x00}; //starting to jump
+const uint8_t MAN_JUMP = 1;
+
+custChar MAN_TOP_A = {0xe,0xe,0x15,0xe,0x4,0xe,0x11,0x00};  //spread eagle
+const uint8_t MAN_TOP= 2;
+
+custChar MAN_LAND_A = {0xe,0xe,0x14,0xe,0x5,0xe,0x9,0x00}; //we have landing
+const uint8_t MAN_LAND = 3;
+
+custChar OBS_A = {0x0,0x0,0x4,0xa,0x15,0xa,0x15,0x00};  //generic obstical
+const uint8_t OBS = 4;
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
@@ -39,6 +57,7 @@ void setup() {
   pinMode(JUMPIN, INPUT);
   // set up the LCD's number of columns and rows: 
   lcd.begin(16, 2);
+  //init all the custom characters
   lcd.createChar(MAN, MAN_A);
   lcd.createChar(MAN_JUMP, MAN_JUMP_A);
   lcd.createChar(MAN_TOP, MAN_TOP_A);
@@ -149,4 +168,10 @@ void loop() {
 	delay(DELAY_TIME);
 	lcd.clear();
 }
-  
+
+/*void increment(); //increments everything in the lcdChar array over one to simulate movement
+void updateDisplay(); //method to update the display to the state of the lcdChar array
+bool isAlive(); //returns if the man is alive or not
+int buttonState(); //returns current state of the button
+int lastState(); //returns last know state
+*/  
